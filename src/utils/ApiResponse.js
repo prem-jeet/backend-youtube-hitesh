4e087ebe1955e67bdbe738370795f61b2b0912c8
prem-jeet prasad <jeet4300795@gmail.com>
2024-06-28 08:52:26 +0000

    ApiResponse util added

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,12 @@
+class ApiResponse {
+    constructor(
+        statusCode,
+        message = "success",
+        data,
+    ){
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
+    }
+}
