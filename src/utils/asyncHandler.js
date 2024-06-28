88be969ffb796b10f16d6b3b3ab844bb768eba3e
prem-jeet prasad <jeet4300795@gmail.com>
2024-06-28 08:45:25 +0000

    async handler added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+/* export default asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+} */
+
+// using promise
+export default asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch(err => next(err))
+    }
+}
